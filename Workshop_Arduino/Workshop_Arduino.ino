03776926e8252e85d94c8ea555b5c30e38e1025a
JoshCornau <cornaujosh@yahoo.de>
2017-11-10 13:54:04 +0100

    PullUp

--- Workshop_Arduino/Workshop_Arduino.ino
+++ Workshop_Arduino/Workshop_Arduino.ino
@@ -1,9 +1,39 @@
+const int buttonPin = 2;
+const int ledPin1 = 7;
+const int ledPin2 = 8;
+
+
+int buttonState = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+  pinMode(ledPin1,OUTPUT);
+  pinMode(ledPin2,OUTPUT);
+  pinMode(buttonPin,INPUT_PULLUP);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+
+  //check the pushbutton
+  if (buttonState == HIGH){
+    ledOff();
+  }
+  else{
+    ledOn();
+  }
 
 }
+
+void ledOn(){
+  digitalWrite(ledPin1,HIGH);
+  digitalWrite(ledPin2,HIGH);
+}
+void ledOff(){
+  digitalWrite(ledPin1,LOW);
+  digitalWrite(ledPin2,LOW);
+}
+
